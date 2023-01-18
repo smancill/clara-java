smancill/clara-java

commit 0d027f50da8265a143802d240a66a78276bfff0a

Migrate some classes to records

clara-std/src/main/java/org/jlab/clara/std/orchestrators/Benchmark.java
@@ -55,7 +55,7 @@ class Benchmark {
         if (r != null) {
             return r.totalTime - r.initialTime;
         }
-        throw new OrchestratorException("Invalid runtime report: missing " + service.name);
+        throw new OrchestratorException("Invalid runtime report: missing " + service.name());
     }
 
     private static ServiceInfo key(ServiceName service) {

clara-core/src/main/java/org/jlab/clara/sys/DpeConfig.java
@@ -6,30 +6,7 @@
 
 package org.jlab.clara.sys;
 
-class DpeConfig {
-
-    private final int maxCores;
-    private final int poolSize;
-    private final long reportPeriod;
-
-    DpeConfig(int maxCores, int poolSize, long reportPeriod) {
-        this.maxCores = maxCores;
-        this.poolSize = poolSize;
-        this.reportPeriod = reportPeriod;
-    }
-
-    int maxCores() {
-        return maxCores;
-    }
-
-    int poolSize() {
-        return poolSize;
-    }
-
-    long reportPeriod() {
-        return reportPeriod;
-    }
-
+record DpeConfig(int maxCores, int poolSize, long reportPeriod) {
 
     static int calculatePoolSize(int cores) {
         int halfCores = cores / 2;

clara-msg/src/test/java/org/jlab/clara/msg/sys/RegistrarTest.java
@@ -321,15 +321,7 @@ public class RegistrarTest {
     }
 
 
-    private static final class ResultAssert {
-
-        private final String valueName;
-        private final OwnerType regType;
-
-        private ResultAssert(String valueName, OwnerType regType) {
-            this.valueName = valueName;
-            this.regType = regType;
-        }
+    private record ResultAssert(String valueName, OwnerType regType) {
 
         private void assertThat(String data,
                                 Set<RegData> result,

clara-shell/src/main/java/org/jlab/clara/std/cli/SystemCommandBuilder.java
@@ -82,15 +82,7 @@ class SystemCommandBuilder {
     }
 
 
-    private static final class Token {
-
-        private final String value;
-        private final boolean split;
-
-        private Token(String value, boolean split) {
-            this.value = value;
-            this.split = split;
-        }
+    private record Token(String value, boolean split) {
 
         @Override
         public String toString() {

clara-std/src/main/java/org/jlab/clara/std/orchestrators/ApplicationInfo.java
@@ -61,7 +61,7 @@ class ApplicationInfo {
     }
 
     private Set<ClaraLang> parseLanguages() {
-        return allServices().map(s -> s.lang).collect(Collectors.toSet());
+        return allServices().map(ServiceInfo::lang).collect(Collectors.toSet());
     }
 
     private Stream<ServiceInfo> allServices() {

clara-std/src/main/java/org/jlab/clara/std/orchestrators/BenchmarkPrinter.java
@@ -22,7 +22,7 @@ class BenchmarkPrinter {
         Logging.info("Benchmark results:");
         printService(application.getReaderService(), "READER");
         for (ServiceInfo service : application.getDataProcessingServices()) {
-            printService(service, service.name);
+            printService(service, service.name());
         }
         printService(application.getWriterService(), "WRITER");
         printTotal();

clara-std/src/main/java/org/jlab/clara/std/orchestrators/CallbackInfo.java
@@ -66,69 +66,7 @@ final class CallbackInfo {
     }
 
 
-    static final class RingTopic {
-
-        final String state;
-        final String session;
-        final String engine;
-
-        RingTopic(String state, String session, String engine) {
-            this.state = state;
-            this.session = session;
-            this.engine = engine;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((engine == null) ? 0 : engine.hashCode());
-            result = prime * result + ((session == null) ? 0 : session.hashCode());
-            result = prime * result + ((state == null) ? 0 : state.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (!(obj instanceof RingTopic other)) {
-                return false;
-            }
-            if (engine == null) {
-                if (other.engine != null) {
-                    return false;
-                }
-            } else if (!engine.equals(other.engine)) {
-                return false;
-            }
-            if (session == null) {
-                if (other.session != null) {
-                    return false;
-                }
-            } else if (!session.equals(other.session)) {
-                return false;
-            }
-            if (state == null) {
-                if (other.state != null) {
-                    return false;
-                }
-            } else if (!state.equals(other.state)) {
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        public String toString() {
-            return "RingTopic [state=" + state + ", session=" + session + ", engine=" + engine
-                    + "]";
-        }
-    }
+    record RingTopic(String state, String session, String engine) { }
 
 
     interface RingListener {

clara-std/src/main/java/org/jlab/clara/std/orchestrators/CoreOrchestrator.java
@@ -60,16 +60,16 @@ class CoreOrchestrator {
 
     void deployService(DeployInfo service) {
         try {
-            ContainerName containerName = service.name.container();
+            ContainerName containerName = service.name().container();
             if (!userContainers.contains(containerName)) {
                 deployContainer(containerName);
                 userContainers.add(containerName);
             }
-            base.deploy(service.name, service.classPath).withPoolsize(service.poolSize).run();
-            userServices.put(service.name, service);
+            base.deploy(service.name(), service.classPath()).withPoolsize(service.poolSize()).run();
+            userServices.put(service.name(), service);
         } catch (ClaraException e) {
             String errorMsg = String.format("failed request to deploy service = %s  class = %s",
-                                            service.name, service.classPath);
+                                            service.name(), service.classPath());
             throw new OrchestratorException(errorMsg, e);
         }
     }
@@ -339,13 +339,7 @@ class CoreOrchestrator {
     }
 
 
-    private static class DpeCallbackWrapper implements GenericCallback {
-
-        final DpeCallBack callback;
-
-        DpeCallbackWrapper(DpeCallBack callback) {
-            this.callback = callback;
-        }
+    private record DpeCallbackWrapper(DpeCallBack callback) implements GenericCallback {
 
         @Override
         public void callback(String data) {

commit 4e7fbc3bc127fa9ac900514640d217b91a3cd26f

std: fix standard orchestrator benchmark report
Broken by 0d027f50 (Migrate some classes to records, 2022-01-04), which
refactored ServiceInfo as a record. That resulted in its equals() and
hashCode() methods now using the classpath field when originally it was
ignored.

But the Benchmark class used the ServiceInfo objects obtained from
ApplicationInfo as keys of its internal map, and to access the values it
was constructing dummy ServiceInfo objects from a ServiceName, with an
empty classpath. Since the keys are records now, even if the services
are present in the map, the key is not found due to originally being
stored with a full classpath and the getter using a key with an empty
classpath.

Fix by using a proper custom class for the keys, with only the required
fields, ignoring the classpath.

clara-std/src/main/java/org/jlab/clara/std/orchestrators/Benchmark.java
@@ -6,6 +6,7 @@
 
 package org.jlab.clara.std.orchestrators;
 
+import org.jlab.clara.base.ClaraLang;
 import org.jlab.clara.base.ServiceName;
 import org.jlab.clara.base.ServiceRuntimeData;
 
@@ -17,16 +18,18 @@ import java.util.Set;
 
 class Benchmark {
 
+    private record ServiceKey(String engine, String container, ClaraLang lang) { }
+
     private static class Runtime {
         long initialTime = 0;
         long totalTime = 0;
     }
 
-    private final Map<ServiceInfo, Runtime> runtimeStats = new HashMap<>();
+    private final Map<ServiceKey, Runtime> runtimeStats = new HashMap<>();
 
     Benchmark(ApplicationInfo application) {
         var services = allServices(application);
-        services.forEach(s -> runtimeStats.put(s, new Runtime()));
+        services.forEach(s -> runtimeStats.put(key(s), new Runtime()));
     }
 
     private static List<ServiceInfo> allServices(ApplicationInfo application) {
@@ -56,18 +59,22 @@ class Benchmark {
     }
 
     long time(ServiceInfo service) {
-        Runtime r = runtimeStats.get(service);
+        Runtime r = runtimeStats.get(key(service));
         if (r != null) {
             return r.totalTime - r.initialTime;
         }
         throw new OrchestratorException("Invalid runtime report: missing " + service.name());
     }
 
-    private static ServiceInfo key(ServiceRuntimeData serviceData) {
+    private static ServiceKey key(ServiceInfo service) {
+        return new ServiceKey(service.name(), service.cont(), service.lang());
+    }
+
+    private static ServiceKey key(ServiceRuntimeData serviceData) {
         return key(serviceData.name());
     }
 
-    private static ServiceInfo key(ServiceName service) {
-        return new ServiceInfo("", service.container().name(), service.name(), service.language());
+    private static ServiceKey key(ServiceName service) {
+        return new ServiceKey(service.name(), service.container().name(), service.language());
     }
 }